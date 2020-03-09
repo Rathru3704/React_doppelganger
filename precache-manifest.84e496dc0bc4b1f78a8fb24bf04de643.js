c763d38a31b364bb9a3922fa198d6be3725f0ae5
Rathru3704 <chan0433@algonquinlive.com>
2020-03-09 00:49:56 -0400

    Updates

---
+++ precache-manifest.84e496dc0bc4b1f78a8fb24bf04de643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14679a884f3a9052cedf412a0065cabe",
+    "url": "/React_doppelganger/index.html"
+  },
+  {
+    "revision": "517da7ebe30f0403f159",
+    "url": "/React_doppelganger/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3ccffedd962d7f58fea8",
+    "url": "/React_doppelganger/static/css/main.23916dc9.chunk.css"
+  },
+  {
+    "revision": "517da7ebe30f0403f159",
+    "url": "/React_doppelganger/static/js/2.e48789fb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React_doppelganger/static/js/2.e48789fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ccffedd962d7f58fea8",
+    "url": "/React_doppelganger/static/js/main.17fee40c.chunk.js"
+  },
+  {
+    "revision": "28b25c30aa556b029681",
+    "url": "/React_doppelganger/static/js/runtime-main.05e89802.js"
+  }
+]);
