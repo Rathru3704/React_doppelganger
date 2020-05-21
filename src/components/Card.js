cc632e47e4bed7580310fd96b8a24a69772edf83
Rathru3704 <chan0433@algonquinlive.com>
2020-05-21 14:49:39 -0400

    Update

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,7 +13,7 @@ const Card = ({id, name, email, phone, address}) => {
         <div className='dib br3 bw2 pt4 ph4 ma2 orange bg-near-black shadow-4 grow'>
         {/* dib = display:inline-block; br3 = border-radius:.5rem; bw2 = border-width:.25rem;
             pa3 = padding:var(--spacing-medium); ma2 = margin:var(--spacing-small); shadow-5 = box-shadow:4px 4px 8px 0px rgba( 0, 0, 0, 0.2 ) */}
-            <img className='br-100' alt='doppelganger' src={`https://randomuser.me/api/portraits/men/${id}.jpg`} style={{width: 250}}></img>
+            <img className='br-100' alt='doppelganger' src={`https://randomuser.me/api/portraits/men/${id}.jpg`} style={{width: 250}}></img>  {// style - outside bracket is a javascript expression, inside one is an object with CSS style}
             <h3 className='mv4'>{name}</h3>
             <div className='tl'>
                 <p><em><strong>Email:</strong></em><br></br>{email}</p>
